38ad4bb4cf08bdc4f1079e1f07e935c964009308
Towerthousand <dani.torramilans@gmail.com>
2016-02-14 05:30:39 +0100

    Styling

--- gameapp/views.py
+++ gameapp/views.py
@@ -22,7 +22,7 @@ def login(request):
             else:
                 bad_login = True
     else:
-        form = forms.LoginForm()
+        form = forms.LoginForm().as_p()
 
     return render(request, 'login.html', {'form': form, 'bad_login': bad_login})
 
@@ -43,7 +43,7 @@ def register(request):
                 auth.login(request, user)
                 return HttpResponseRedirect("/")
     else:
-        form = forms.LoginForm()
+        form = forms.LoginForm().as_p()
 
     return render(request, 'register.html', {'form': form, 'bad_username': bad_username})
 
